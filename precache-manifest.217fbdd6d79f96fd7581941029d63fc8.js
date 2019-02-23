cf1b0a87184099bb7050011fd00201d16409095f
graphicskart <ramkumar897003@gmail.com>
2019-02-23 23:25:14 +0530

    Updates

---
+++ precache-manifest.217fbdd6d79f96fd7581941029d63fc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a4ab24b90403852fc26c99406e4b0a5",
+    "url": "/infographics-bootstrap-auth-forms/static/media/person.7a4ab24b.jpg"
+  },
+  {
+    "revision": "7f43c900e804634721d45d1a1c6b1bf1",
+    "url": "/infographics-bootstrap-auth-forms/static/media/google.7f43c900.png"
+  },
+  {
+    "revision": "3736e14d94bf1755a0c9",
+    "url": "/infographics-bootstrap-auth-forms/static/js/runtime~main.3736e14d.js"
+  },
+  {
+    "revision": "f7012bc767e9afbbdc59",
+    "url": "/infographics-bootstrap-auth-forms/static/js/main.f7012bc7.chunk.js"
+  },
+  {
+    "revision": "609d19394363a5e5620b",
+    "url": "/infographics-bootstrap-auth-forms/static/js/2.609d1939.chunk.js"
+  },
+  {
+    "revision": "f7012bc767e9afbbdc59",
+    "url": "/infographics-bootstrap-auth-forms/static/css/main.16371f3e.chunk.css"
+  },
+  {
+    "revision": "240ad00a1403c9a93086ea65e6d6b6f2",
+    "url": "/infographics-bootstrap-auth-forms/index.html"
+  }
+];
